14242d3d9520ee22751bc8bdd2e19fc615d460f2
luinor223 <vile2040@gmail.com>
2023-07-19 20:27:05 +0700

    Added comparision_count to some places

--- Sorting Library/shakerSort.cpp
+++ Sorting Library/shakerSort.cpp
@@ -1,68 +1,77 @@
 /**
- * Shaker Sort
+ * Shaker Sort (This version is taken from Dr. Nguyen Ngoc Thao's lecture notes)
  * Average Case: O(n^2)
  * Best Case: O(n)
  * Worst Case: O(n^2)
  * Space complexity: O(1)
-*/
-
-void shaker_sort(int a[], int n) {
-    int left = 1, right = n - 1, k = 1; 
-    do {
+ */
+void shaker_sort(int a[], int n)
+{
+    int left = 1, right = n - 1, k = 1;
+    do
+    {
         // loop from left to right same as
         // the bubble sort
-        for (int j = left; j <= right; j++) {
-            if (a[j - 1] > a[j]) {
+        for (int j = left; j <= right; j++)
+        {
+            if (a[j - 1] > a[j])
+            {
                 swap(a[j - 1], a[j]);
-                k = j;  //Save the position of the last exchange
+                k = j; // Save the position of the last exchange
             }
-        }   //Larger elements to the end
+        } // Larger elements to the end
 
-        //As all the items after k is in correct order,
-        //move the 'right' to k - 1
-        right = k - 1; 
+        // As all the items after k is in correct order,
+        // move the 'right' to k - 1
+        right = k - 1;
 
         // from right to left, doing the
         // same comparison as in the previous stage
-        for (int j = right; j >= left; j--) {
-            if (a[j - 1] > a[j]) {
+        for (int j = right; j >= left; j--)
+        {
+            if (a[j - 1] > a[j])
+            {
                 swap(a[j - 1], a[j]);
-                k = j;  //Save the position of the last exchange
+                k = j; // Save the position of the last exchange
             }
-        }   //Smaller elements to the top
+        } // Smaller elements to the top
 
-        //As all the items before k is in correct order, 
-        //move the 'left' to k + 1
+        // As all the items before k is in correct order,
+        // move the 'left' to k + 1
         left = k + 1;
 
     } while (left <= right);
 }
 
-void shaker_sort_with_counter(int a[], int n, long long &comparision_count) {
+void shaker_sort_with_counter(int a[], int n, long long &comparision_count)
+{
     comparision_count = 0;
-    int left = 1, right = n - 1, k = 1; 
-    do {
-        // loop from left to right same as
-        // the bubble sort
-        for (int j = left; j <= right; j++) {
-            if (++comparision_count && a[j - 1] > a[j]) {
+    int left = 1, right = n - 1, k = 1;
+    do
+    {
+        // loop from left to right same as the bubble sort
+        for (int j = left; ++comparision_count && j <= right; j++)
+        {
+            if (++comparision_count && a[j - 1] > a[j])
+            {
                 swap(a[j - 1], a[j]);
-                k = j;  //Save the position of the last exchange
+                k = j; // Save the position of the last exchange
             }
-        }   //Larger elements to the end
+        } // Larger elements to the end
 
-        right = k - 1;  //As all the items after k is in correct order, move the right to k - 1
+        right = k - 1; // As all the items after k is in correct order, move the right to k - 1
 
-        // from right to left, doing the
-        // same comparison as in the previous stage
-        for (int j = right; j >= left; j--) {
-            if (++comparision_count && a[j - 1] > a[j]) {
+        // From right to left, doing the same comparison as in the previous stage
+        for (int j = right; ++comparision_count && j >= left; j--)
+        {
+            if (++comparision_count && a[j - 1] > a[j])
+            {
                 swap(a[j - 1], a[j]);
-                k = j;  //Save the position of the last exchange
+                k = j; // Save the position of the last exchange
             }
-        }   //Smaller elements to the top
+        } // Smaller elements to the top
 
-        left = k + 1; //As all the items before k is in correct order, move the left to k + 1
+        left = k + 1; // As all the items before k is in correct order, move the left to k + 1
 
-    } while (left <= right);
+    } while (++comparision_count && left <= right);
 }
